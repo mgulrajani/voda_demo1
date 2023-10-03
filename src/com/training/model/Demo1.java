03b742bac4149ab3880848c5ff25d7e8f26fa529
mgulrajani <muskaangulrajani@hotmail.com>
2023-10-03 16:46:29 +0530

    Assignment in demo1

--- src/com/training/model/Demo1.java
+++ src/com/training/model/Demo1.java
@@ -2,6 +2,17 @@ package com.training.model;
 
 public class Demo1 {
 public static void main(String[] args) {
-	
+	/**
+	 * create a Student class with id ,name , batchid
+	 * 
+	 * create a Batch class with id , programtitle , startdate and enddate --LocalDate
+	 * 
+	 * create a app , where you will create 3 students in an array 
+	 * 
+	 * and display them
+	 * 
+	 * 
+	 * 
+	 */
 }	
 }
