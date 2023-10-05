9b3c286e3c611007de72a14eb36d3ef62549983a
mgulrajani <muskaangulrajani@hotmail.com>
2023-10-05 17:40:33 +0530

    assignment 2

--- src/com/training/apps/Assignments.java
+++ src/com/training/apps/Assignments.java
@@ -10,6 +10,13 @@ package com.training.apps;
  * 4)create a class PrintableApp , in that create static print method which takes Printable object and call the method
  * 5)create a Map<Integer, Book>
  * 
+ * 
+ * ===
+ * 
+ * 1)index.html , create input age , submit invoke a servlet checkAgeServlet
+ * 
+ * 2)create user model username and password , repo login (retrieval)  and register  ,you are adding a user , methods ,service  --will consume repo (has a relationship) login and register
+ * create app - will consume service (has a relationship)
  */
 public class Assignments {
 
